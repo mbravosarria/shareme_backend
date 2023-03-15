49810f12e6608ef45ebaeb61ed2d7156f27c35eb
mbravosarria <mbravosarria@gmail.com>
2023-03-15 16:39:35 -0400

    feat: shareme sanity.io

---
+++ schemas/user.ts
@@ -0,0 +1,22 @@
+export default {
+  name: 'user',
+  type: 'document',
+  title: 'User',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'picture',
+      title: 'Picture',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+  ],
+}
